2cb5f1537642dc05a20d01ff705efa047b7ddd6d
cmojarro <cmo@711-64.local>
2015-03-19 12:33:48 -0500

    adding conversion in element jquery
    found a resource in stack overflow that helps me with the code
    conversion. I know theres a tool out there that does it automatically,
    how can there not?@#$

--- test/cesarExample.js
+++ test/cesarExample.js
@@ -21,12 +21,26 @@ describe('Cesar testing validators', function () {
 		it('should return false when passed a value', function () {
 				assert.isFalse(validators.required());
 		});// it empty
-
-	describe('lengthAtLeast', fucntion () {
-			it('should return true when passed a a string with 5 letters', fucntion () {
-				assert.isTrue()
-			});
 	});
 
-	});//required description for 
+	describe('lengthAtLeast()', function () {
+			it('return true when passed string with 5 letters', function () {
+				assert.isTrue(validators.lengthAtLeast('Chicago', 5));
+			}); //return lengthatleast
+
+			it('retun false when values fail to be 4', function () {
+				assert.isFalse(validators.lengthAtLeast([1, 4], 4));
+			});
+
+	});//required description for lengthAtleast
+
+	describe('lengthAtMost()', function() {
+			it('return true when strings are < then ten ', function () {
+				assert.isTrue(validators.lengthAtMost('Password', 10));
+			});
+
+			it('return false when array is > 6 indexes', function () {
+				assert.isFalse(validators.lengthAtMost([1, 2,'Fixing', 4, 5, 6, 7], 6));
+			});
+	})
 });
